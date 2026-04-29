ff29239d7211e7e022345075a200c27182800048
toya <emontoyas1@eafit.edu.co>
2026-04-29 17:38:13 -0500

    pending-inicializando y plantenado parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,5 +1,31 @@
 #include "parser.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int parser(){
+    FILE* dataset = fopen("data/roadNet-PA.txt", "r");
+   if(!dataset){
+    cout<<"error al abrir archivo."<<endl;
+    return 1;
+}
+char line[199];
+while(fgets(line, sizeof(line), dataset)){
+    if (line[0], == '#') continue;
+    int u,v;
+sscanf(line,"%d %d", &u, &v);
 
 
 
 
+
+
+}
+
+
+
+
+
+
+}
