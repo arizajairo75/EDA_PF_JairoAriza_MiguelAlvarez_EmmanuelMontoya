2a47eedb1cdcf4e77dc40bfe73afa287ed30d90f
toya <emontoyas1@eafit.edu.co>
2026-04-30 06:21:11 -0500

    progress:avances en el parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -2,30 +2,34 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <unordered_map>
+#include <vector>
 using namespace std;
+
+
 
 int parser(){
     FILE* dataset = fopen("data/roadNet-PA.txt", "r");
-   if(!dataset){
-    cout<<"error al abrir archivo."<<endl;
-    return 1;
+
+
+    if(!dataset) {
+        cout<<"error al abrir archivo."<<endl;
+        return 1;
+    }
+
+    char line[199];
+    unordered_map<int,int> id_nodo;
+    int i=0;
+    while(fgets(line, sizeof(line), dataset)){
+        int u,v;
+        if (line[0] == '#') continue;
+        sscanf(line,"%d %d", &u, &v);
+
+
+
+        printf("%d %d\n",u,v);
 }
-char line[199];
-while(fgets(line, sizeof(line), dataset)){
-    if (line[0], == '#') continue;
-    int u,v;
-sscanf(line,"%d %d", &u, &v);
 
 
-
-
-
-
-}
-
-
-
-
-
-
+return 0;
 }
