bdfa88bcc1445b129b57e31f7fba49525eb7b971
Jairo Ariza <arizajairo75@gmail.com>
2026-05-06 15:03:42 -0500

    fix: corregir addEdge y agregar getVecinos y resize

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,15 +1,17 @@
 //
 // Created by monto on 04/05/2026.
-//
-
+// edited by jairo on 06/05/2026.
 #include "graph.h"
 
-    void graph::addEdge(int u, int v, int peso) {
-        adj[u].push_back({peso,peso});
-        adj[v].push_back({u,peso});
-    }
+void graph::addEdge(int u, int v, int peso) {
+    adj[u].push_back({v, peso});
+    adj[v].push_back({u, peso});
+}
 
+void graph::resize(int n) {
+    adj.resize(n);
+}
 
-
-
-
+const std::vector<std::pair<int,int>>& graph::getVecinos(int nodo) const {
+    return adj[nodo];
+}
