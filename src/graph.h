642ccaa2356ce3541837aedd5403de4f27b4b573
Jairo Ariza <arizajairo75@gmail.com>
2026-05-06 14:43:55 -0500

    fix: corregir graph.h y graph.cpp, agregar gitignore

--- src/graph.h
+++ src/graph.h
@@ -1,24 +1,26 @@
 //
-// Created by monto on 04/05/2026.
-//
-#include <vector>
+// edited by jairo on 06/05/2026.
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <vector>
+#include <unordered_map>
+#include <utility>
 
 class graph {
-
 private:
     std::vector<std::vector<std::pair<int,int>>> adj;
 
-
 public:
-    graph(int numNodos) {                 //constructor
-        adj.resize(numNodos);
-    }
+    int numNodes;
+    int numEdges;
+    std::unordered_map<int,int> idMap;
+
+    graph() : numNodes(0), numEdges(0) {}
+
     void addEdge(int u, int v, int peso);
+    void resize(int n);
+    const std::vector<std::pair<int,int>>& getVecinos(int nodo) const;
 };
 
-
-
-#endif //GRAPH_H
+#endif
